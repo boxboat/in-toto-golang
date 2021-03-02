a154ed1d129b99ebd7f82fec5f09bf1923b9619a
Cole Kennedy <colek42@gmail.com>
2021-03-02 16:27:36 -0600

    get certs

--- test-infra/mint-cert.sh
+++ test-infra/mint-cert.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
-
-
 docker exec test-infra_spire-server_1 \
 /opt/spire/bin/spire-server x509 mint \
 -registrationUDSPath /run/spire/sockets/spire-registration.sock \
--spiffeID spiffe://spire.boxboat.io/$1
+-spiffeID spiffe://spire.boxboat.io/$1 \
+-write .
+
+docker exec test-infra_spire-server_1 cat $1-svid.pem > $1-svid.pem
+docker exec test-infra_spire-server_1 cat $1-key.pem > $1-key.pem
+docker exec test-infra_spire-server_1 cat $1-bundle.pem > $1-bundle.pem
