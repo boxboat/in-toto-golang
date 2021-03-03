d5ffa813c4986ee223957e947d5bdf506c2880e2
Mikhail Swift <mikhail@boxboat.com>
2021-03-02 21:36:21 -0500

    More debug code removal

--- in_toto/verifylib.go
+++ in_toto/verifylib.go
@@ -521,9 +521,7 @@ func VerifyLinkSignatureThesholds(layout Layout,
 				}
 
 				// test against the root pool with the key's certificate if it has any
-				fmt.Println("testing certs")
 				if err := VerifyCertificateTrust(cert, rootCertPool, intermediateCertPool); err == nil {
-					fmt.Println(err)
 					linksPerStepVerified[signerKeyID] = linkMb
 				}
 			}
